748cdd40a089993750327fb0ebbc988dd7ee64de
MatthewAlvarez5 <alvarezmatthew2002@gmail.com>
2023-05-23 16:45:33 -0700

    Gotta make another stupid ass project because the database is all jank

--- COMP003B.FinalProject/Models/GameDeveloper.cs
+++ COMP003B.FinalProject/Models/GameDeveloper.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public int GameId { get; set; }
         public int DeveloperId { get; set; }
-        public int GenreId { get; set; }
+        public int GenreId { get; set; } // ligma
 
         public virtual Game? Game { get; set; }
         public virtual Developer? Developer { get; set; }
